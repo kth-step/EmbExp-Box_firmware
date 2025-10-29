9e9f4bc58701891a0ae45943801c6790e40be401
Andreas Lindner <andili@kth.se>
2025-10-29 20:30:36 +0100

    Add Raspberry Pi 5 custom boot code

---
+++ custom_src/rpi5-boot/src/main.c
@@ -0,0 +1,71 @@
+#include "uart.h"
+
+
+static void dbg_puts(const char *s)
+{
+	while (*s)
+		uart_putchar(*s++);
+}
+
+static void dbg_loopback()
+{
+	while (1) {
+		uart_putchar(uart_getchar());
+	}
+}
+
+
+const int flagmem = 0;
+
+volatile int* flagp = &flagmem;
+int main_core0()
+{
+	uart_init();
+	dbg_puts("Booting board: rpi5\n");
+
+	dbg_puts("Compilation at: ");
+	dbg_puts(__DATE__);
+	dbg_puts(" (");
+	dbg_puts(__TIME__);
+	dbg_puts(")\n");
+
+	dbg_puts("Init complete: #0\n");
+	//dbg_loopback();
+
+        *flagp = *flagp + 1;
+
+	return 0;
+}
+
+int main_core1()
+{
+	while(*flagp != 1);
+
+	dbg_puts("Init complete: #1\n");
+
+        *flagp = *flagp + 1;
+
+	return 0;
+}
+
+int main_core2()
+{
+	while(*flagp != 2);
+
+	dbg_puts("Init complete: #2\n");
+
+        *flagp = *flagp + 1;
+
+	return 0;
+}
+
+int main_core3()
+{
+	while(*flagp != 3);
+
+	dbg_puts("Init complete: #3\n");
+
+        *flagp = *flagp + 1;
+
+	return 0;
+}
